ddaae17e33412ba808d405cd4c62dd7a3bb0d91c
ajkarimi7 <ajkarimi7@gmail.com>
2019-05-02 06:47:08 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 function theBeatlesPlay(musicians, instruments) {
   var arr = [];
   for (let i = 0, l = musicians.length; i < l; i++) {
-    arr.push(`${musicians} plays ${instruments}`)
+    arr.push(`${musicians[i]} plays ${instruments[i]}`)
   }
 }
